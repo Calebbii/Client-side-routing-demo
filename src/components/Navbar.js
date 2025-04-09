f26bcb1840e41f6b6db3af957e293ea696f3702c
Calebbii <biicaleb316@gmail.com>
2025-04-09 10:42:02 +0300

    Created the Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../css/navbar.css'
+import { NavLink } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>
+                    <NavLink exact to="/home">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink exact to="/about">About Us</NavLink>  
+                </li>
+                <li>
+                    <NavLink exact to="/contact">Conatct Us</NavLink>
+                </li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
